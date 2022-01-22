618b4f916a75727a01c29bd5964e47877eede378
hieupd19 <hieupd19@ghtk.co>
2022-01-22 14:49:01 +0700

    rename swagger config

--- src/main/java/com/example/demo/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/infrastructure/config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerCfg {
+public class SwaggerConfig {
 
   @Bean
   public Docket api() {
