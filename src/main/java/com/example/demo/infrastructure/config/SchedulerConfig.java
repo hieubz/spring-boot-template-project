5180ebcf349dda1a782b83f786d1e88ebb9d6a93
hieupd19 <hieupd19@ghtk.co>
2022-01-29 21:09:08 +0700

    background job demo

---
+++ src/main/java/com/example/demo/infrastructure/config/SchedulerConfig.java
@@ -0,0 +1,8 @@
+package com.example.demo.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {}
---
+++ src/main/java/com/example/demo/infrastructure/jobs/ShowCurrentTimeSchedule.java
@@ -0,0 +1,25 @@
+package com.example.demo.infrastructure.jobs;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ShowCurrentTimeSchedule {
+
+  private static final DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+  @Scheduled(initialDelay = 1000, fixedDelay = 2000)
+  public void showCurrentTime() {
+    Date d = new Date();
+    System.out.println(df.format(d));
+  }
+
+  @Scheduled(cron = "0 0 0 14 2 ?", zone = "Asia/Saigon")
+  public void doSomethingInValentineDay() {
+    System.out.println("go for a walk");
+  }
+}
