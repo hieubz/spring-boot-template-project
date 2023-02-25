92203998e932785cb1435b2d0e64f2591ab189d9
hieubz <phamduyhieuit@gmail.com>
2023-02-25 16:52:29 +0700

    CORS configuration

--- README.md
+++ README.md
@@ -41,7 +41,7 @@ And we have the code flow as shown below:
 - [x] Retry
 - [x] ActiveMQ and Spring JMS Integration
 - [x] Export XLSX/CSV reports
-- [ ] CORS configuration
+- [x] CORS configuration
 - [x] Swagger
 
 #### Advanced Techniques
@@ -81,13 +81,16 @@ Then you can apply the code of the corresponding commit for your feature.
 
 ## Setup
 
-To run this project (optional for new guys, **minimum 16GB RAM**)
+To run this project (optional for new guys, **minimum 16GB RAM** if you run local **Elasticsearch**)
 
 - Download and install [IntelliJ IDEA](https://www.jetbrains.com/idea/download/) (Recommends)
 - Install [Docker and Docker Compose](https://linuxhint.com/install-docker-compose-ubuntu-22-04/) if you don't have your own database and message brokers
 - Run `git clone https://github.com/hieubz/spring-boot-based-project.git`
 - [Configure](https://www.jetbrains.com/help/idea/sdk.html#change-project-sdk) **JDK 11** for your project on **IntelliJ IDEA**
 - Run `docker-compose up -d` to start **MySQL**, **MongoDB**, **Redis**, **Kafka**, **ActiveMQ** and **Elasticsearch** (or update your database configs in **application.properties**)
+  
+    _Notes: **Elasticsearch** container requires much RAM on local_
+
 - Run **_resources/schema/mysql-schema.sql_** to initialize your MySQL database.
 - Run commands in **_resources/schema/mongo-schema.txt_** to initialize your MongoDB database.
 - Run your **DemoApplication** and check Swagger UI: http://localhost:8080/swagger-ui/index.html#/
--- src/main/java/com/example/demo/infrastructure/config/auth/WebSecurityConfig.java
+++ src/main/java/com/example/demo/infrastructure/config/auth/WebSecurityConfig.java
@@ -13,6 +13,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -56,7 +62,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     if (applicationType.equals(WEB_APPLICATION_TYPE)) {
-      http.cors().and().csrf().disable();
+      http.cors().configurationSource(corsConfigurationSource()).and().csrf().disable();
       http.authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll().anyRequest().authenticated();
       http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
       http.exceptionHandling().authenticationEntryPoint(authEntryPointJwt);
@@ -64,5 +70,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
           fixedTokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
       http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    // update your allowed domains here
+    configuration.setAllowedOrigins(List.of("https://phamduyhieu.com"));
+    configuration.setAllowedHeaders(
+            Arrays.asList(
+                    "Authorization",
+                    "Content-Type",
+                    "Cache-Control",
+                    "Verify-Token",
+                    "apikey"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
