961eaf7e14c0c9fcb4ed86197307223e3a00d505
hieubz <phamduyhieuit@gmail.com>
2023-03-18 09:37:20 +0700

    Scheduling configuration

--- src/main/java/com/example/demo/infrastructure/config/SchedulerConfig.java
+++ src/main/java/com/example/demo/infrastructure/config/SchedulerConfig.java
@@ -1,8 +1,10 @@
 package com.example.demo.infrastructure.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@ConditionalOnProperty(prefix = "com.example.demo.scheduling", name="enabled", havingValue="true", matchIfMissing = true)
 public class SchedulerConfig {}
