f4b0575ae6fcc696b80ebb3be9d041233a29b562
hieubz <phamduyhieuit@gmail.com>
2024-04-28 16:16:44 +0700

    Kafka blocking retry

--- src/main/java/com/example/demo/infrastructure/config/kafka/KafkaConsumerConfig.java
+++ src/main/java/com/example/demo/infrastructure/config/kafka/KafkaConsumerConfig.java
@@ -1,5 +1,8 @@
 package com.example.demo.infrastructure.config.kafka;
 
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,10 +12,11 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.util.backoff.BackOff;
+import org.springframework.util.backoff.FixedBackOff;
 
 @EnableKafka
 @Configuration
@@ -23,6 +27,12 @@ public class KafkaConsumerConfig {
 
   @Value("${kafka.consumer.config.group-id}")
   private String groupId;
+
+  @Value(value = "${kafka.backoff.interval}")
+  private Long interval;
+
+  @Value(value = "${kafka.backoff.max_failure}")
+  private Long maxAttempts;
 
   @Bean
   public ConsumerFactory<String, String> consumerFactory() {
@@ -43,6 +53,35 @@ public class KafkaConsumerConfig {
     factory.setConsumerFactory(consumerFactory());
     // receive the entire batch of consumer records received from the consumer poll
     factory.setBatchListener(true);
+    // If a retry policy is present, we’ll set the ack mode to AckMode.RECORD
+    // to make sure that the consumer will redeliver messages if an error happens during processing.
+    factory.setCommonErrorHandler(blockingRetryErrorHandler());
+    factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.RECORD);
     return factory;
+  }
+
+  /**
+   * In the blocking retry, when message processing fails, the consumer blocks until the retry mechanism finishes its retries,
+   * or until the maximum number of retries is reached.
+   * => Keep the order of messages, however it causes delays.
+   * Notes:
+   * - consumerRecord: represents the Kafka record that caused the error.
+   * - exception: represents the exception that was thrown.
+   * - If we don’t set any retryable exceptions, the default set of retryable exceptions will be used:
+   *   MessagingException
+   *   RetryableException
+   *   ListenerExecutionFailedException
+   *
+   */
+  @Bean
+  public DefaultErrorHandler blockingRetryErrorHandler() {
+    BackOff fixedBackOff = new FixedBackOff(interval, maxAttempts);
+    DefaultErrorHandler errorHandler = new DefaultErrorHandler((consumerRecord, e) -> {
+              // logic to execute when all the retry attempts are failed
+    }, fixedBackOff);
+    // specify which exceptions are retryable and which are non-retryable.
+    errorHandler.addRetryableExceptions(SocketTimeoutException.class);
+    errorHandler.addNotRetryableExceptions(NullPointerException.class);
+    return errorHandler;
   }
 }
